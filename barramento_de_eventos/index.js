5b7a41a6371790d2fcd1712bcfbcacf711c921f9
João Victor <joaov_rc@outlook.com>
2021-09-21 19:53:18 -0300

    Barramentom

---
+++ barramento_de_eventos/index.js
@@ -0,0 +1,16 @@
+const express = require ("express")
+const bodyParser = require('body-parser')
+const axios = require('axios')
+
+const app = express()
+app.use(bodyParser.json())
+
+app.post('/eventos', (req, res) =>{
+    const evento = req.body
+    axios.post('http://localhost:4000/eventos', evento)
+    axios.post('http://localhost:5000/eventos', evento)
+
+    res.status(204).end()
+})
+
+app.listen(10000, () => "Barramento de eventos. Porta 10000.")
